22c11850061487d7a67a595c31c83eb906382a29
Mayur Ghule <54388290+mayurghule494@users.noreply.github.com>
2023-05-27 21:38:08 +0530

    Update experience_data.js

--- src/experience/data/experience_data.js
+++ src/experience/data/experience_data.js
@@ -1,34 +1,26 @@
 export const experienceDataCollection = [
   {
-    startDate: "April 11, 2022",
+    startDate: "May, 2022",
     endDate: "Present",
-    title: "Software Developer",
+    title: "Senior DevOps Engineer",
     details:
-      "1. Work with different types of Flutter Apps<br/><br/>2. Made some apps from scratch<br/><br/>3. Made APIs that use in full-scale Flutter projects<br/><br/>4. Managing a team as team leader<br/>and more...",
-    company: "Aim2Excel",
+      "1. Deploying an Application to the Lower environments using Jenkins<br/><br/>2. And Production environment using Ansible on AWS EC2<br/><br/>3. Managing Nginx web servers and load balancers to ensure high availability and performance of web applications.<br/>and more...",
+    company: "LTIMindtree Ltd for Dyson Client",
   },
   {
-    startDate: "January 8, 2022",
-    endDate: "April 8, 2022",
-    title: "Flutter Developer Intern",
+    startDate: "Jul, 2021",
+    endDate: "May, 2022",
+    title: "Senior DevOps Engineer",
     details:
-      "1. Automatic Theme Changer With State Management<br/><br/>2. Connect With Backend and API Integration<br/><br/>3. Worked With Pagination Concept<br/><br/>4. Make Some Apps from Scratch With Others<br/>and more...",
-    company: "Aim2Excel",
+      "Deployed an application to production using Blue Green deployment with the help of tool like Repliweb, f5, Perforce on On-premises Windows servers.",
+    company: "LTIMindtree Ltd for Homestead Client",
   },
   {
-    startDate: "Jun 1, 2021",
-    endDate: "Jul 3, 2021",
-    title: "Mobile Application Developer Intern",
+    startDate: "Jan 2017",
+    endDate: "Feb 2020",
+    title: "Process Associate (Jr. DevOps Engineer)",
     details:
-      "Works on Social Authentication, Payment Gateway Integration, Data Store with SQLite",
-    company: "The Sparks Foundation",
-  },
-  {
-    startDate: "March 2021",
-    endDate: "June 2021",
-    title: "Open Source Particiant",
-    details:
-      "Contributed More Than 5 Projects to Open Source. Works with Email and Social Authentication, Complex App Screen Designing, Connect with Backend and More...",
-    company: "GirlScript Summer of Code",
+      "Worked on AWS and DevOps tools based on tickets using JIRA. Worked on AWS services like EC2, VPC, ELB, Auto-Scaling, Route53, S3, IAM. Created Master Slave architecture using Jenkins and More...",
+    company: "Genpact India pvt ltd",
   },
 ];
